9e962c46465295a78621109f3e70efec8f568711
w <supakitu@yandex.ru>
2026-06-15 13:36:04 +0300

    Added: getCarInfo(), Car[] cars, Scanner scanner
    Signed-off-by: w <supakitu@yandex.ru>

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,2 +1,9 @@
 public class Car {
+    public final String name;
+    public final int speed;
+
+    public Car(String name, int speed) {
+        this.name = name;
+        this.speed = speed;
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,23 @@
+import java.util.Scanner;
 
 public class Main {
+    Car[] cars = new Car[3];
+    static final Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("======================================================================\n" +
+                           "                          24 часа Ле-Мана                              \n" +
+                           "                               v1.0                                   \n" +
+                           "======================================================================\n");
+
+    }
+
+    public static void getCarInfo() {
+        while (true) {
+            for (int i = 0; i < 3; i++) {
+                System.out.println("- Введите имя машины №" + i + ":");
+            }
+        }
+
     }
 }

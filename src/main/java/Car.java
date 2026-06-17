50caae24d6527edc3bfa1b46e7701f49a7a9e461
w <supakitu@yandex.ru>
2026-06-17 15:34:14 +0300

    После review

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,42 +1,9 @@
-import java.util.Scanner;
-
 public class Car {
     final String name;
     final int speed;
-    private static final Scanner sc = new Scanner(System.in);
 
     public Car(String name, int speed) {
         this.name = name;
         this.speed = speed;
-    }
-
-    public static Car createCar(int carNumber) {
-        String name;
-        int speed;
-
-        System.out.println("- Введите имя машины №" + carNumber + ":");
-        name = sc.next();
-
-        while (true) {
-            System.out.println("- Введите скорость машины №" + carNumber + ":");
-
-            // Проверяем что ввод это целое число (и оно в диапозоне 1 - 250), если нет цикл продолжает вертется [узнал про hasNextInt() в интернете]
-            if (sc.hasNextInt()) {
-                speed = sc.nextInt();
-
-                if (speed > 0 && speed <= 250) {
-                    break;
-                } else {
-                    System.out.println("Скорость автомобиля должна быть больше 0 км/ч и не превышать 250 км/ч.");
-                }
-            } else {
-                // после одной итерации цикла с неверным вводом, метод hasNextInt() почему то запоминал это значением и превращял метод в бесконечный
-                // поэтому я взял у LLM эту строчку
-                sc.next();
-                System.out.println("Скорость автомобиля должна быть целым числом.");
-            }
-        }
-
-        return new Car(name, speed);
     }
 }
